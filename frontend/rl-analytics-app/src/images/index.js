17c727f56d5b216f9e4aada29549991ddae78a63
Matthew O'Callaghan <matthewocallaghan1998@hotmail.co.uk>
2020-02-19 14:27:42 +0000

    Added rank images

---
+++ frontend/rl-analytics-app/src/images/index.js
@@ -0,0 +1,43 @@
+import GrandChampion from './ranks/GrandChampion.png';
+import ChampionIII from './ranks/ChampionIII.png';
+import ChampionII from './ranks/ChampionII.png';
+import ChampionI from './ranks/ChampionI.png';
+import DiamondIII from './ranks/DiamondIII.png';
+import DiamondII from './ranks/DiamondII.png';
+import DiamondI from './ranks/DiamondI.png';
+import PlatinumIII from './ranks/PlatinumIII.png';
+import PlatinumII from './ranks/PlatinumII.png';
+import PlatinumI from './ranks/PlatinumI.png';
+import GoldIII from './ranks/GoldIII.png';
+import GoldII from './ranks/GoldII.png';
+import GoldI from './ranks/GoldI.png';
+import SilverIII from './ranks/SilverIII.png';
+import SilverII from './ranks/SilverII.png';
+import SilverI from './ranks/SilverI.png';
+import BronzeIII from './ranks/BronzeIII.png';
+import BronzeII from './ranks/BronzeII.png';
+import BronzeI from './ranks/BronzeI.png';
+import Unranked from './ranks/Unranked.png';
+
+export const Ranks = {
+    GrandChampion,
+    ChampionIII,
+    ChampionII,
+    ChampionI,
+    DiamondIII,
+    DiamondII,
+    DiamondI,
+    PlatinumIII,
+    PlatinumII,
+    PlatinumI,
+    GoldIII,
+    GoldII,
+    GoldI,
+    SilverIII,
+    SilverII,
+    SilverI,
+    BronzeIII,
+    BronzeII,
+    BronzeI,
+    Unranked
+}

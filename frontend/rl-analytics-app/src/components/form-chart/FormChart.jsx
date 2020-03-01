8fbe24c396e41f1e4a895e56cab5e6dc94446f63
Matthew O'Callaghan <matthewocallaghan1998@hotmail.co.uk>
2020-03-01 20:24:50 +0000

    Restricted form chart y axis to integers

--- frontend/rl-analytics-app/src/components/form-chart/FormChart.jsx
+++ frontend/rl-analytics-app/src/components/form-chart/FormChart.jsx
@@ -61,6 +61,9 @@ const FormChart = ({ players }) => {
                             display: true, 
                             labelString: 'MMR', 
                             fontSize: 15
+                        },
+                        ticks: {
+                            precision: 0
                         }
                     }]
                 }
